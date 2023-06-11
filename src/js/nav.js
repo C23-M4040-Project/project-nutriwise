ed3f1d12f262ff4308ececa1321df96284e32ef5
Rafi <rafipersonalporto@gmail.com>
2023-06-11 12:07:21 +0700

    Update File Menu Page Navbar Bug Fixing

--- src/js/nav.js
+++ src/js/nav.js
@@ -14,6 +14,20 @@ function closemenu() {
   close.style.display = "none";
 }
 
+function handleResize() {
+  if (window.innerWidth >= 768) {
+    menu.style.left = "0";
+    open.style.display = "none";
+    close.style.display = "none";
+  } else {
+    menu.style.left = "-100%";
+    open.style.display = "block";
+    close.style.display = "none";
+  }
+}
+
+window.addEventListener("resize", handleResize);
+
 // nav background color change
 
 function change() {
